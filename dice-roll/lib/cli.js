2c6d0e06e3d5e1a56f5b49c164a035e3498b4416
Sarah Morris <sarah.morris9@gmail.com>
2018-03-06 15:21:13 -0600

    working on parsing arguments

--- dice-roll/lib/cli.js
+++ dice-roll/lib/cli.js
@@ -2,9 +2,12 @@
 
 process.title = 'Dice Roll'
 
+const { randomInt } = require('./math.js')
 const { argv: [,, ...args] } = process
-const { count, sides } = require('./parse-args')(args)
+const parseArgs = require('./parse-args.js')
 const { roll, toDiceNotation } = require('./dice')
+
+let args = parseArgs(argv);
 
 const dice = toDiceNotation({count, sides})
 const total = roll(dice)
--- dice-roll/lib/parse-args.js
+++ dice-roll/lib/parse-args.js
@@ -1,10 +1,23 @@
 #!/usr/bin/env node
-
 'use strict';
 
-module.exports = (argsArray) => {
-    let dieNums = {};
-    dieNums.count // do stuff
-    dieNums.sides // do stuff
-    return dieNums
+module.exports = (argv) => {
+    let count, sides;
+    let [,,...args] = argv;
+    if (args.length == 0) {
+        count = 1;
+        sides = 6;
+    } else if (args.length == 1) {
+        count = 1;
+        sides = parseInt(args[0]);
+    } else if (args.length == 2) {
+        count = parseInt(args[0]);
+        sides = parseInt(args[1]);
+    }
+    if (count != count || sides != sides) {
+        return false;
+    } else {
+        return { count, sides };
+    }
 };
+
