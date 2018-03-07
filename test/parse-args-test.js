b8e4ff70b10f1374515692fc911b2e3a4831b27e
Sarah Morris <sarah.morris9@gmail.com>
2018-03-07 15:38:32 -0600

    Completed exercise 15

--- test/parse-args-test.js
+++ test/parse-args-test.js
@@ -1,2 +1,11 @@
-const { assert: {isFunction} } = require('chai');
-const { dieNums } = require('../dice-roll/lib/parse-args');
+const { assert: {isFunction, isObject, isNumber} } = require('chai');
+const { num, sides } = require('../dice-roll/lib/parse-args');
+const parseArgs = require('../dice-roll/lib/parse-args');
+
+describe('parse-args module', () => {
+    describe('parseArgs', () => {
+        it('should be a function', () => {
+            isFunction(parseArgs);
+        })
+    })
+})
